bc90b04b1147b21130e6a2faaea098d9ae06752b
nurfad7 <nurfad7@gmail.com>
2024-10-18 06:22:31 +0700

    fix: about page image sources switched

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -9,9 +9,9 @@ interface AboutProps {
 const About: FC<AboutProps> = ({setIsShow}) => {
   return (
     <div id="about" 
-      className='w-full min-h-[150vh] md:min-h-[100vh] flex flex-col justify-center px-20'>
+      className='w-full min-h-[100vh] md:min-h-[100vh] flex flex-col justify-center px-20'>
       <div className="hidden md:block relative z-10 md:text-9xl font-bold leading-[0.5] pt-20">ABOUT</div>
-      <div className="relative z-20 flex flex-col md:flex-row w-full h-full md:h-80">
+      <div className="relative z-20 flex flex-col md:flex-row w-full min-h-[42rem] md:min-h-80">
         <div className="flex flex-col w-full md:w-[20%] gap-5 md:gap-0">
           <div className="md:text-9xl font-bold flex"><span className="block md:hidden">ABOUT&nbsp;</span>US</div>
           <div className="text-sm text-left mb-5 md:mb-0">
@@ -19,14 +19,14 @@ const About: FC<AboutProps> = ({setIsShow}) => {
             Every space we touch becomes a reflection of our dedication to detail.&quot;
           </div>
         </div>
-        <div className="flex w-full md:w-[50%] h-auto md:h-full mb-2 md:mb-0">
-          <Image src="https://res.cloudinary.com/dhu6ayhdj/image/upload/f_auto,q_auto/v1/pallete/iyc2zdszdsfvdb5oyxu9" 
+        <div className="flex w-full md:w-[50%] h-auto md:h-80 mb-2 md:mb-0">
+          <Image src="https://res.cloudinary.com/dhu6ayhdj/image/upload/f_auto,q_auto/v1/pallete/mjfslnr0qub7ccv6qkzl" 
             alt="photo by koncept" width={2560} height={1707}
             className="w-full h-full object-cover rounded-2xl" />
         </div>
         <div className="flex flex-col w-full md:w-[30%] pl-5">
           <div className="flex w-full h-24 mb-5">
-            <Image src="https://res.cloudinary.com/dhu6ayhdj/image/upload/v1729064880/pallete/mjfslnr0qub7ccv6qkzl.jpg" 
+            <Image src="https://res.cloudinary.com/dhu6ayhdj/image/upload/f_auto,q_auto/v1/pallete/iyc2zdszdsfvdb5oyxu9" 
               alt="photo by dean fugate" width={3208} height={4010}
               className="w-full h-full object-cover object-top rounded-2xl" />
           </div>
@@ -36,7 +36,7 @@ const About: FC<AboutProps> = ({setIsShow}) => {
             Over the years, we&apos;ve expanded our services to cover everything from residential to commercial projects, 
             always focusing on quality and customer satisfaction.&quot;
           </div>
-          <div onClick={setIsShow} className="font-bold text-sm cursor-pointer">Learn more</div>
+          <div onClick={setIsShow} className="font-bold text-sm cursor-pointer mb-5 md:mb-0">Learn more</div>
         </div>
       </div>
       <div className="md:flex flex-col gap-2">
